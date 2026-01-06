6428c40bf9a7f8142828da6a3dde38893d6b7b69
Ohanael <htinlinn.116900@gmail.com>
2026-01-07 03:07:59 +0700

    Added a few more updates to the code for better structure and readability. - created two new functions: RemoveCharacter and AppendCharacter. - added more tests in the Test function for now.

--- Program.cs
+++ Program.cs
@@ -27,69 +27,52 @@ public class Program
         {
             if (c == '#')
             {
-                if (sequence.Length > 0)
-                {
-                    char key = sequence[0];
-                    if (NumToLetter.TryGetValue(key, out var letters))
-                    {
-                        int index = (sequence.Length - 1) % letters.Length;  // % to account for the letters cycling
-                        output.Append(letters[index]);
-                    }
-                    sequence.Clear();
-                }
+                AppendCharacter(output, sequence);
                 break;
             }
 
             if (c == ' ')
             {
-                if (sequence.Length > 0)
-                {
-                    char key = sequence[0];
-                    if (NumToLetter.TryGetValue(key, out var letters))
-                    {
-                        int index = (sequence.Length - 1) % letters.Length;  // % to account for the letters cycling
-                        output.Append(letters[index]);
-                    }
-                    sequence.Clear();
-                }
+                AppendCharacter(output, sequence);
                 continue;
             }
 
             if (c == '*')
             {
-                if (sequence.Length > 0)
-                {
-                    char key = sequence[0];
-                    if (NumToLetter.TryGetValue(key, out var letters))
-                    {
-                        int index = (sequence.Length - 1) % letters.Length;  // % to account for the letters cycling
-                        output.Append(letters[index]);
-                    }
-                    sequence.Clear();
-                }
-
-                if (output.Length > 0)
-                    output.Length--;
-
+                AppendCharacter(output, sequence);
+                RemoveCharacter(output);
                 continue;
             }
 
             if (sequence.Length > 0 && sequence[0] != c)
-            {
-                char key = sequence[0];
-                if (NumToLetter.TryGetValue(key, out var letters))
-                {
-                    int index = (sequence.Length - 1) % letters.Length; // % to account for the letters cycling
-                    output.Append(letters[index]);
-                }
-                sequence.Clear();
-            }
+                AppendCharacter(output, sequence);
 
             sequence.Append(c);
         }
 
         return output.ToString();
     }
+
+    public static void AppendCharacter(StringBuilder output, StringBuilder sequence)
+    {
+        if (sequence.Length > 0)
+        {
+            char key = sequence[0];
+            if (NumToLetter.TryGetValue(key, out var letters))
+            {
+                int index = (sequence.Length - 1) % letters.Length;  // % to account for the letters cycling
+                output.Append(letters[index]);
+            }
+            sequence.Clear();
+        }
+    }
+
+    public static void RemoveCharacter(StringBuilder output)
+    {
+        if (output.Length > 0)
+            output.Length--;
+    }
+
 
     public static void Main(string[] args)
     {
@@ -101,9 +84,18 @@ public class Program
     public static void Test()
     {
         // Test cases
-        Console.WriteLine(OldPhonePad("33#"));
-        Console.WriteLine(OldPhonePad("227*#"));
-        Console.WriteLine(OldPhonePad("4433555 555666#"));
-        Console.WriteLine(OldPhonePad("8 88777444666*664#"));
+        Console.WriteLine(OldPhonePad("2#"));                    // A
+        Console.WriteLine(OldPhonePad("222#"));                  // C
+        Console.WriteLine(OldPhonePad("2222#"));                 // A
+        Console.WriteLine(OldPhonePad("2 2 2#"));                // AAA
+        Console.WriteLine(OldPhonePad("6665553#"));              // OLD
+        Console.WriteLine(OldPhonePad("4433555*#"));             // HE
+        Console.WriteLine(OldPhonePad("*2#"));                   // A
+        Console.WriteLine(OldPhonePad("999666880277733#"));      // YOU ARE
+        Console.WriteLine(OldPhonePad("77773333**#"));           // ""
+        Console.WriteLine(OldPhonePad("8 88777444666*664#"));    // TURING
+        Console.WriteLine(OldPhonePad("44 33 555 555 6660#"));   // HELLO
+        Console.WriteLine(OldPhonePad("4433555 555666096667775553#")); // HELLO WORLD
+
     }
 }
